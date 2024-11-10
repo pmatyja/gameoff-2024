2922ba965e25ccde8f4e7ac225e9500c94efd787
Piotr Matyja <peter.matyja@gmail.com>
2024-11-10 23:13:31 +0100

    Bounds builder creates box colliders around the level

--- Assets/unity-core/Runtime/Components/BoundsBuilder.cs
+++ Assets/unity-core/Runtime/Components/BoundsBuilder.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Lavgine
@@ -11,6 +12,11 @@ namespace Lavgine
 
         private void Start()
         {
+            this.MakeWall(new Vector3(+1, +0, +0));
+            this.MakeWall(new Vector3(-1, +0, +0));
+            this.MakeWall(new Vector3(+0, +0, +1));
+            this.MakeWall(new Vector3(+0, +0, -1));
+            this.MakeWall(new Vector3(+0, -1, +0));
         }
 
         private void OnDrawGizmos()
@@ -30,6 +36,25 @@ namespace Lavgine
             (
                 this.transform.position + this.bounds.center - new Vector3(0.0f, this.bounds.extents.y * 0.5f + 0.1f, 0.0f), 
                 new Vector3(this.bounds.extents.x, 0.1f, this.bounds.extents.z)
+            );
+        }
+
+        private void MakeWall(Vector3 axis)
+        {
+            var collider = this.gameObject.AddComponent<BoxCollider>();
+
+            collider.center = new Vector3
+            (
+                ( axis.x * (this.bounds.extents.x * 0.5f) ) + this.bounds.center.x + (axis.x * 0.5f), 
+                ( axis.y * (this.bounds.extents.y * 0.5f) ) + this.bounds.center.y + (axis.y * 0.5f), 
+                ( axis.z * (this.bounds.extents.z * 0.5f) ) + this.bounds.center.z + (axis.z * 0.5f)
+            );
+
+            collider.size = new Vector3
+            (
+                axis.x + ( ( 1 - MathF.Abs(axis.x) ) * this.bounds.extents.x), 
+                axis.y + ( ( 1 - MathF.Abs(axis.y) ) * this.bounds.extents.y), 
+                axis.z + ( ( 1 - MathF.Abs(axis.z) ) * this.bounds.extents.z)
             );
         }
     }
