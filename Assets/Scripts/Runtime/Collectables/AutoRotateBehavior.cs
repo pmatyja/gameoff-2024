c6f4e217800c0ab3e49564df40a035166699e860
OwenCooperSFX <owencoopersfx@gmail.com>
2024-11-11 10:01:19 -0800

    Make basic collectable a gold coin for originality.

--- Assets/Scripts/Runtime/Collectables/AutoRotateBehavior.cs
+++ Assets/Scripts/Runtime/Collectables/AutoRotateBehavior.cs
@@ -5,9 +5,9 @@ public class AutoRotateBehavior : MonoBehaviour
 {
     [Tooltip("Degrees per second, per axis")]
     [field: SerializeField] public Vector3 RotationSpeed { get; private set; } = new Vector3(0, 360, 0);
+    [field: SerializeField] public Vector3 RandomStartRange { get; private set; } = Vector3.zero;
 
     private bool _rotate;
-    
     private Vector3 _startRotation;
 
     private void OnDisable()
@@ -20,6 +20,16 @@ public class AutoRotateBehavior : MonoBehaviour
     private void Start()
     {
         _startRotation = transform.rotation.eulerAngles;
+        
+        if (RandomStartRange != Vector3.zero)
+        {
+            transform.Rotate(new Vector3(
+                UnityEngine.Random.Range(-RandomStartRange.x, RandomStartRange.x),
+                UnityEngine.Random.Range(-RandomStartRange.y, RandomStartRange.y),
+                UnityEngine.Random.Range(-RandomStartRange.z, RandomStartRange.z)
+            ));
+        }
+        
         _rotate = true;
     }
     
