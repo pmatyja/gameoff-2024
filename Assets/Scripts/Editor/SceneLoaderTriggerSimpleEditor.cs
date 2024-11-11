61ba2b1168e6c70e7653387ffc69545bcd227263
OwenCooperSFX <owencoopersfx@gmail.com>
2024-11-10 23:41:58 -0800

    WIP second level blockout.

---
+++ Assets/Scripts/Editor/SceneLoaderTriggerSimpleEditor.cs
@@ -0,0 +1,50 @@
+﻿using Runtime.World;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(SceneLoaderTriggerSimple))]
+    public class SceneLoaderTriggerSimpleEditor : UnityEditor.Editor
+    {
+        private SerializedProperty _showDebug;
+        private bool _showDebugValue;
+        
+        public override void OnInspectorGUI()
+        {
+            var sceneLoaderTrigger = (SceneLoaderTriggerSimple)target;
+            
+            _showDebug = serializedObject.FindProperty(nameof(_showDebug));
+
+            // Draw the script field
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.ObjectField("Script", MonoScript.FromMonoBehaviour(sceneLoaderTrigger), typeof(MonoScript), false);
+            EditorGUI.EndDisabledGroup();
+
+            // Get the list of scenes in the build settings
+            var scenes = EditorBuildSettings.scenes;
+            var sceneNames = new string[scenes.Length];
+            for (var i = 0; i < scenes.Length; i++)
+            {
+                sceneNames[i] = System.IO.Path.GetFileNameWithoutExtension(scenes[i].path);
+            }
+
+            // Draw the dropdown list
+            var selectedIndex = Mathf.Max(0, System.Array.IndexOf(sceneNames, sceneLoaderTrigger.SceneName));
+            selectedIndex = EditorGUILayout.Popup("Scene", selectedIndex, sceneNames);
+            sceneLoaderTrigger.SceneName = sceneNames[selectedIndex];
+            
+            // Draw the player tag field
+            sceneLoaderTrigger.PlayerTag = EditorGUILayout.TagField("Player Tag", sceneLoaderTrigger.PlayerTag);
+
+            // Draw the debug field
+            _showDebugValue = _showDebug.boolValue = EditorGUILayout.Toggle("Show Debug", _showDebugValue);
+            
+            // Save changes
+            if (GUI.changed)
+            {
+                EditorUtility.SetDirty(sceneLoaderTrigger);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/World/SceneLoaderTriggerSimple.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using OCSFX.Utility.Debug;
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Runtime.World
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class SceneLoaderTriggerSimple : MonoBehaviour
+    {
+        public string SceneName;
+        public string PlayerTag = "Player";
+
+        [Header("Debug")]
+        [SerializeField] private bool _showDebug;
+        
+        private BoxCollider _boxCollider;
+
+        private void Awake()
+        {
+            _boxCollider.isTrigger = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            OCSFXLogger.Log($"{nameof(OnTriggerEnter)} : {other.name}", this, _showDebug);
+            
+            if (other.CompareTag(PlayerTag))
+            {
+                OCSFXLogger.Log($"{PlayerTag} entered the trigger.", this, _showDebug);
+                
+                SceneManager.LoadScene(SceneName);
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(transform.position + _boxCollider.center, _boxCollider.size);
+        }
+
+        private void OnValidate()
+        {
+            if (!_boxCollider) _boxCollider = GetComponent<BoxCollider>();
+        }
+
+        private void Reset()
+        {
+            _boxCollider = GetComponent<BoxCollider>();
+            _boxCollider.isTrigger = true;
+        }
+    }
+}
