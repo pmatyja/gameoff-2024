19a8110eeaa88a07b043923e194a302ab5021d13
OwenCooperSFX <owencoopersfx@gmail.com>
2024-11-11 20:39:23 -0800

    Improvement to ExitDoor

--- Assets/Scripts/Runtime/Interactions/ExitDoor.cs
+++ Assets/Scripts/Runtime/Interactions/ExitDoor.cs
@@ -19,6 +19,8 @@ namespace Runtime.Interactions
             {
                 OCSFXLogger.LogError($"{nameof(BlockMoverScript)} not found on {nameof(ExitDoor)} GameObject ({name})", this, _showDebug);
             }
+            
+            UpdateCanInteract();
         }
         
         private void OnEnable()
@@ -33,33 +35,35 @@ namespace Runtime.Interactions
             ItemInventory.OnItemRemoved -= OnItemRemoved;
         }
 
-        private void OnItemAdded(CollectableData addedItem)
-        {
-            OCSFXLogger.Log($"[{name}] Item added: {addedItem.name}", this, _showDebug);
-            
-            CanInteract = ItemInventory.Instance.ContainsAll(_requiredCollectables);
+        private void OnItemAdded(CollectableData addedItem) => UpdateCanInteract();
 
-            if (CanInteract)
-            {
-                OCSFXLogger.Log($"[{name}] Can now interact with {name}", this, _showDebug);
-            }
-        }
-
-        private void OnItemRemoved(CollectableData addedItem)
-        {
-            CanInteract = ItemInventory.Instance.ContainsAll(_requiredCollectables);
-            
-            if (!CanInteract)
-            {
-                OCSFXLogger.Log($"[{name}] Can no longer interact with {name}", this, _showDebug);
-            }
-        }
+        private void OnItemRemoved(CollectableData addedItem) => UpdateCanInteract();
 
         protected override void Open()
         {
             base.Open();
         
             _blockMoverScript.Activate();
+        }
+        
+        private void UpdateCanInteract()
+        {
+            var currentInteractState = CanInteract;
+            
+            CanInteract = ItemInventory.Instance.ContainsAll(_requiredCollectables);
+            
+            if (currentInteractState != CanInteract)
+            {
+                DebugLog();
+            }
+        }
+
+        private void DebugLog()
+        {
+            OCSFXLogger.Log(
+                CanInteract
+                    ? $"[{name}] Can now interact with {name}"
+                    : $"[{name}] Can no longer interact with {name}", this, _showDebug);
         }
     }
 }
