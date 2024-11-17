fcc30c188f92ce66bafda9555442e6b6a2897bb8
OwenCooperSFX <owencoopersfx@gmail.com>
2024-11-16 17:19:52 -0800

    Create UniqueID script (GUID.ToString()) to use as a potential way to track all the non-unique collectables.

---
+++ Assets/Scripts/Runtime/Utility/GameOff2024UniqueID.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using OCSFX.Attributes;
+using OCSFX.Utility.Attributes;
+using UnityEngine;
+
+namespace Runtime.Utility
+{
+    public class GameOff2024UniqueID : MonoBehaviour
+    {
+        [field: SerializeField, ReadOnly] public string ID { get; private set; }
+
+#if UNITY_EDITOR
+        [SerializeField, Button(nameof(GenerateID))]
+        private bool _generateIdButton;
+#endif
+        
+        public void GenerateID() => ID = Guid.NewGuid().ToString();
+
+        private void Reset() => GenerateID();
+    }
+}
