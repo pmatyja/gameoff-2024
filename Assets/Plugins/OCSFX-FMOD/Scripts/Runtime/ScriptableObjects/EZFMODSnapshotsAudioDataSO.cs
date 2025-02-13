f07a51c0f3c0ad8ba77a18bcc267baf740dcdb20
Owen Cooper <owencoopersfx@gmail.com>
2025-02-13 15:11:18 -0800

    Update snapshots audio data for EZFMOD.

--- Assets/Plugins/OCSFX-FMOD/Scripts/Runtime/ScriptableObjects/EZFMODSnapshotsAudioDataSO.cs
+++ Assets/Plugins/OCSFX-FMOD/Scripts/Runtime/ScriptableObjects/EZFMODSnapshotsAudioDataSO.cs
@@ -1,10 +1,9 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using FMODUnity;
 using OCSFX.EZFMOD.Components;
 using OCSFX.EZFMOD.Types;
 using OCSFX.EZFMOD.Debug;
+using OCSFX.EZFMOD.Utility.Generics;
 using UnityEngine;
 
 namespace OCSFX.EZFMOD.ScriptableObjects
@@ -14,25 +13,25 @@ namespace OCSFX.EZFMOD.ScriptableObjects
     {
         // Fields
         [Header("Mix State Snapshots")]
-        [SerializeField] private List<FMODEvent> _states = new List<FMODEvent>()
+        [SerializeField] private List<SerializedKeyValuePair<string, EZFMODSnapshot>> _stateSnapshots = new()
         {
-            new FMODEvent("None", new EventReference()),
-            new FMODEvent("Dialog", new EventReference()),
-            new FMODEvent("PauseMenu", new EventReference()),
-            new FMODEvent("MainMenu", new EventReference()),
-            new FMODEvent("Narration", new EventReference()),
-            new FMODEvent("Silence", new EventReference())
+            new SerializedKeyValuePair<string, EZFMODSnapshot>("None", null),
+            new SerializedKeyValuePair<string, EZFMODSnapshot>("Silence", null),
+            new SerializedKeyValuePair<string, EZFMODSnapshot>("MainMenu", null),
+            new SerializedKeyValuePair<string, EZFMODSnapshot>("PauseMenu", null),
+            new SerializedKeyValuePair<string, EZFMODSnapshot>("Cutscene", null),
+            new SerializedKeyValuePair<string, EZFMODSnapshot>("Dialogue", null),
         };
 
         [Header("Reverb Snapshots")]
-        [SerializeField] private List<FMODEvent> _reverbs = new List<FMODEvent>()
+        [SerializeField] private List<SerializedKeyValuePair<string, EZFMODSnapshot>> _reverbSnapshots = new()
         {
-            new FMODEvent("None", new EventReference()),
-            new FMODEvent("Interior_SmallRoom", new EventReference()),
-            new FMODEvent("Interior_MediumRoom", new EventReference()),
-            new FMODEvent("Interior_LargeRoom", new EventReference()),
-            new FMODEvent("Interior_Hallway", new EventReference()),
-            new FMODEvent("Exterior", new EventReference())
+            new SerializedKeyValuePair<string, EZFMODSnapshot>("None", null),
+            new SerializedKeyValuePair<string, EZFMODSnapshot>("Exterior", null),
+            new SerializedKeyValuePair<string, EZFMODSnapshot>("Interior_SmallRoom", null),
+            new SerializedKeyValuePair<string, EZFMODSnapshot>("Interior_MediumRoom", null),
+            new SerializedKeyValuePair<string, EZFMODSnapshot>("Interior_LargeRoom", null),
+            new SerializedKeyValuePair<string, EZFMODSnapshot>("Interior_Hallway", null)
         };
         
         private readonly HashSet<EZFMODReverbZoneBase> _reverbZoneSet = new HashSet<EZFMODReverbZoneBase>();
@@ -40,8 +39,8 @@ namespace OCSFX.EZFMOD.ScriptableObjects
         private EZFMODReverbZoneBase _currentReverbZone;
 
         // Properties
-        public List<FMODEvent> States => _states;
-        public List<FMODEvent> Reverbs => _reverbs;
+        public List<SerializedKeyValuePair<string, EZFMODSnapshot>> States => _stateSnapshots;
+        public List<SerializedKeyValuePair<string, EZFMODSnapshot>> Reverbs => _reverbSnapshots;
 
         // Methods
 
@@ -94,36 +93,36 @@ namespace OCSFX.EZFMOD.ScriptableObjects
         
         private void StartReverbZone(EZFMODReverbZoneBase reverbZone)
         {
-            if (_reverbs.Count < 1) return;
-            
-            var reverbSnapshot = _reverbs.Find(reverb => reverb.Name == reverbZone.ReverbName);
-            if (reverbSnapshot.EventRef.IsNull) return;
+            if (!TryGetReverbSnapshot(reverbZone.ReverbName, out var reverbSnapshot)) return;
 
             OCSFXLogger.Log($"Start Reverb: Name: {reverbZone.ReverbName}", this, _showDebug);
             
-            reverbSnapshot.EventRef.Play2D();
+            reverbSnapshot.Play2D();
         }
         
         private void StopReverbZone(EZFMODReverbZoneBase reverbZone)
         {
-            if (_reverbs.Count < 1) return;
+            if (!TryGetReverbSnapshot(reverbZone.ReverbName, out var reverbSnapshot)) return;
             
-            var reverbEvent = _reverbs.Find(reverb => reverb.Name == reverbZone.ReverbName);
-            if (reverbEvent.EventRef.IsNull) return;
-            
-            reverbEvent.EventRef.StopGlobal();
+            reverbSnapshot.StopAll(true);
         }
         
         public void StopAllReverbs()
         {
-            if (_reverbs.Count > 0)
-                foreach (var reverb in _reverbs) StopSnapshot(reverb.EventRef);
+            foreach (var reverb in _reverbSnapshots)
+            {
+                if (reverb == null) continue;
+                StopReverbSnapshot(reverb.Key);
+            }
         }
 
         public void StopAllStates()
         {
-            if (_states.Count > 0)
-                foreach (var state in _states) StopSnapshot(state.EventRef);
+            foreach (var state in _stateSnapshots)
+            {
+                if (state == null) continue;
+                StopStateSnapshot(state.Key);
+            }
         }
 
         public void ClearAllSnapshots()
@@ -134,68 +133,86 @@ namespace OCSFX.EZFMOD.ScriptableObjects
 
         public void SetStateSnapshot(string stateName, bool state)
         {
-            if (!_states.TryGetEventReference(stateName, out var eventRef))
+            if (!TryGetStateSnapshot(stateName, out var snapshot))
             {
-                OCSFXLogger.LogWarning($"{stateName} was not found in {_states}. Check {this}.", this, _showDebug);
+                OCSFXLogger.LogWarning($"{stateName} was not found in {_stateSnapshots}. Check {this}.", this, _showDebug);
                 return;
             }
 
-            if (state) StartSnapshot(eventRef);
-            else StopSnapshot(eventRef);
+            if (state) StartSnapshot(snapshot);
+            else StopSnapshot(snapshot);
         }
 
         public void SetReverbSnapshot(string reverbName, bool state)
         {
-            if (!_reverbs.TryGetEventReference(reverbName, out var eventRef))
+            if (!TryGetReverbSnapshot(reverbName, out var snapshot))
             {
-                OCSFXLogger.LogWarning($"{reverbName} was not found in {_reverbs}. Check {this}.", this, _showDebug);
+                OCSFXLogger.LogWarning($"{reverbName} was not found in {_reverbSnapshots}. Check {this}.", this, _showDebug);
                 return;
             }
 
-            if (state) StartSnapshot(eventRef);
-            else StopSnapshot(eventRef);
+            if (state) StartSnapshot(snapshot);
+            else StopSnapshot(snapshot);
         }
 
         public void StartStateSnapshot(string eventKey)
         {
-            var snapshot = _states.Find(match => match.Name == eventKey);
-            if (snapshot.EventRef.IsNull) return;
+            if (!TryGetStateSnapshot(eventKey, out var snapshot)) return;
 
-            snapshot.EventRef.Play2D();
+            snapshot.Play2D();
         }
 
         public void StopStateSnapshot(string eventKey)
         {
-            var snapshot = _states.Find(match => match.Name == eventKey);
-            if (snapshot.EventRef.IsNull) return;
+            if (!TryGetStateSnapshot(eventKey, out var snapshot)) return;
 
-            snapshot.EventRef.StopGlobal();
+            snapshot.StopAll(true);
         }
         
         private void StartReverbSnapshot(string eventKey)
         {
-            var snapshot = _reverbs.Find(match => match.Name == eventKey);
-            if (snapshot.EventRef.IsNull) return;
-
-            snapshot.EventRef.Play2D();
+            if (!TryGetReverbSnapshot(eventKey, out var snapshot)) return;
+            
+            snapshot.Play2D();
         }
 
         private void StopReverbSnapshot(string eventKey)
         {
-            var snapshot = _reverbs.Find(match => match.Name == eventKey);
-            if (snapshot.EventRef.IsNull) return;
-
-            snapshot.EventRef.StopGlobal();
+            if (!TryGetReverbSnapshot(eventKey, out var snapshot)) return;
+            
+            snapshot.StopAll(true);
         }
         
-        private static void StartSnapshot(EventReference snapshot)
+        private static void StartSnapshot(EZFMODSnapshot snapshot)
         {
-            if (!snapshot.IsNull) snapshot.Play2D();
+            snapshot?.Play2D();
         }
 
-        private static void StopSnapshot(EventReference snapshot)
+        private static void StopSnapshot(EZFMODSnapshot snapshot)
         {
-            if (!snapshot.IsNull) snapshot.StopGlobal();
+            snapshot?.StopAll(true);
+        }
+        
+        private bool TryGetStateSnapshot(string stateName, out EZFMODSnapshot snapshot)
+        {
+            snapshot = GetStateSnapshot(stateName);
+            return snapshot;
+        }
+        
+        private EZFMODSnapshot GetStateSnapshot(string stateName)
+        {
+            return _stateSnapshots.Find(match => match.Key == stateName)?.Value;
+        }
+        
+        private bool TryGetReverbSnapshot(string reverbName, out EZFMODSnapshot snapshot)
+        {
+            snapshot = GetReverbSnapshot(reverbName);
+            return snapshot;
+        }
+        
+        private EZFMODSnapshot GetReverbSnapshot(string reverbName)
+        {
+            return _reverbSnapshots.Find(match => match.Key == reverbName)?.Value;
         }
     }
 }
