475f1f3520e6ce5288bfe16fccf9583b871fb983
OwenCooperSFX <owencoopersfx@gmail.com>
2025-02-16 20:50:17 -0800

    Fix pickup sound

--- Assets/Scripts/Runtime/Collectables/CollectableData.cs
+++ Assets/Scripts/Runtime/Collectables/CollectableData.cs
@@ -95,7 +95,7 @@ namespace Runtime.Collectables
             
             if (OnCollectSfxEvent)
             {
-                OnCollectSfxEvent.Play(collectableTransform.gameObject);
+                OnCollectSfxEvent.PlayOneShot();
             }
         }
 
